aa6c4ea64a75b5e302cda0c5da1c471cfd22545d
cuixiaorui <cui_xiaorui@126.com>
2020-12-23 23:30:05 +0800

    update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,53 +1,27 @@
-// import * as jest from "@types/jest";
+export {};
 
-declare module jest {
-  interface Matchers<R, T = {}> {
-    /**
-     * @description
-     * Check whether the given wrapper has certain classes within its `class` attribute.
-     *
-     * You must provide at least one class
-     * @example
-     *
-     * Button.vue
-     * <template>
-     *   <button class="el-button--small">
-     *   </button>
-     * </template>
-     *
-     * const wrapper = mount(Button)
-     *
-     * expect(wrapper).toHaveClass(`el-button--small`)
-     * expect(deleteButton).not.toHaveClass(`el-button--foo`)
-     */
-    toHaveClass(className: string): R;
+declare global {
+  namespace jest {
+    interface Matchers<R, T = {}> {
+      /**
+       * @description
+       * Check whether the given wrapper has certain classes within its `class` attribute.
+       *
+       * You must provide at least one class
+       * @example
+       *
+       * Button.vue
+       * <template>
+       *   <button class="el-button--small">
+       *   </button>
+       * </template>
+       *
+       * const wrapper = mount(Button)
+       *
+       * expect(wrapper).toHaveClass(`el-button--small`)
+       * expect(deleteButton).not.toHaveClass(`el-button--foo`)
+       */
+      toHaveClass(className: string): R;
+    }
   }
 }
-
-// export {};
-
-// declare global {
-//   namespace jest {
-//     interface Matchers<R, T = {}> {
-//       /**
-//        * @description
-//        * Check whether the given wrapper has certain classes within its `class` attribute.
-//        *
-//        * You must provide at least one class
-//        * @example
-//        *
-//        * Button.vue
-//        * <template>
-//        *   <button class="el-button--small">
-//        *   </button>
-//        * </template>
-//        *
-//        * const wrapper = mount(Button)
-//        *
-//        * expect(wrapper).toHaveClass(`el-button--small`)
-//        * expect(deleteButton).not.toHaveClass(`el-button--foo`)
-//        */
-//       toHaveClass(className: string): R;
-//     }
-//   }
-// }
