975a7b2c4610cd7da9929284b1412c5c8fbc9ab5
cuixiaorui <cui_xiaorui@126.com>
2020-12-25 17:57:31 +0800

    build: add jest test

---
+++ tests/index.spec.js
@@ -0,0 +1,113 @@
+import "../dist/index.js";
+import { mount } from "@vue/test-utils";
+import Button from "./suite/Button.vue";
+
+describe("matchers", () => {
+  describe("toHaveClass", () => {
+    it("have class", () => {
+      const wrapper = mount(Button);
+      expect(wrapper).toHaveClass("el-button");
+    });
+
+    it("have not class", () => {
+      const wrapper = mount(Button);
+      expect(wrapper).not.toHaveClass("el-foo");
+    });
+  });
+
+  describe("toHaveAttribute", () => {
+    it("have attribute", () => {
+      const wrapper = mount(Button);
+      expect(wrapper).toHaveAttribute("foo");
+    });
+
+    it("specify the specific value", () => {
+      const wrapper = mount(Button);
+      expect(wrapper).toHaveAttribute("foo", "1");
+    });
+
+    it("child dom have attribute", () => {
+      const wrapper = mount(Button);
+      expect(wrapper.get("#core")).toHaveAttribute("disabled");
+    });
+  });
+
+  describe("toBeVisible", () => {
+    it("is visible", () => {
+      const wrapper = mount(Button);
+      expect(wrapper.get("#core")).toBeVisible();
+    });
+
+    it("is not visible", () => {
+      const wrapper = mount(Button);
+      expect(wrapper.get("#testVisible")).not.toBeVisible();
+    });
+
+    it("wrapper must be DomWrapper", () => {
+      const wrapper = mount(Button);
+      expect(() => {
+        expect(wrapper).toBeVisible();
+      }).toThrowError("wrapper must be DomWrapper");
+    });
+  });
+
+  describe("toHaveTextContent", () => {
+    it("have text content", () => {
+      const wrapper = mount(Button);
+      expect(wrapper).toHaveTextContent("button");
+    });
+
+    it("have not text content", () => {
+      const wrapper = mount(Button);
+      expect(wrapper).not.toHaveTextContent("bar");
+    });
+  });
+
+  describe("toBeExist", () => {
+    it("is exist", () => {
+      const wrapper = mount(Button);
+      expect(wrapper.find("#testExist")).toBeExist();
+    });
+
+    it("is not exist", () => {
+      const wrapper = mount(Button);
+      expect(wrapper.find("#testNotExist")).not.toBeExist();
+    });
+  });
+});
+
+describe("vtuPlugins", () => {
+  describe("getByTestId", () => {
+    it("use vueWrapper called ", () => {
+      const wrapper = mount(Button);
+
+      expect(wrapper.getByTestId("forVTU")).toBeTruthy();
+      expect(wrapper.getByTestId("forVTU").text()).toContain("for");
+    });
+
+    it("use domWrapper called ", () => {
+      const wrapper = mount(Button);
+
+      const domWrapper = wrapper.get(".el-button");
+      expect(domWrapper.getByTestId("forVTU")).toBeTruthy();
+      expect(domWrapper.getByTestId("forVTU").text()).toContain("for");
+    });
+  });
+
+  describe("findByTestId", () => {
+    it("use vueWrapper called ", () => {
+      const wrapper = mount(Button);
+
+      expect(wrapper.findByTestId("forVTU")).toBeTruthy();
+      expect(wrapper.findByTestId("forVTU").text()).toContain("for");
+    });
+
+    it("use domWrapper called ", () => {
+      const wrapper = mount(Button);
+
+      const domWrapper = wrapper.get(".el-button");
+      expect(domWrapper.findByTestId("forVTU")).toBeTruthy();
+      expect(domWrapper.findByTestId("forVTU").text()).toContain("for");
+    });
+  });
+});
---
+++ tests/suite/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="el-button el-button-small" foo="1">
+    button
+    <button id="core" disabled></button>
+    <div id="testVisible" v-show="false"></div>
+    <div id="testExist" v-if="true"></div>
+    <div id="testNotExist" v-if="false"></div>
+    <div data-testid="forVTU">for</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
